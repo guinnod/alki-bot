af576e14452203344a039ee07541cdfdb90b0ce2
guinnod <210103064@stu.sdu.edu.kz>
2023-11-26 19:59:43 +0600

    fix: message content changes

--- handlers.py
+++ handlers.py
@@ -6,19 +6,19 @@ PRODUCT_ID, COLOR, SIZE, ADDRESS, ORDER_ID = range(5)
 
 
 async def ask_for_product_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    cancel_button = InlineKeyboardButton("Cancel", callback_data='cancel_order')
+    cancel_button = InlineKeyboardButton("Отмена", callback_data='cancel_order')
     cancel_keyboard = [
         [cancel_button]
     ]
     reply_markup = InlineKeyboardMarkup(cancel_keyboard)
-    await update.message.reply_text("Send me the ID of the product", reply_markup=reply_markup)
+    await update.message.reply_text("Напишите номер товара.", reply_markup=reply_markup)
     return PRODUCT_ID
 
 
 async def ask_for_color(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     colors = get_colors_from_api('http://localhost:5000')
 
-    cancel_button = InlineKeyboardButton("Cancel", callback_data='cancel_order')
+    cancel_button = InlineKeyboardButton("Отмена", callback_data='cancel_order')
     color_keyboard = [
         [InlineKeyboardButton("Red", callback_data='color_red')],
         [InlineKeyboardButton("Black", callback_data='color_black')],
@@ -27,13 +27,13 @@ async def ask_for_color(update: Update, context: ContextTypes.DEFAULT_TYPE) -> i
     ]
     reply_markup = InlineKeyboardMarkup(color_keyboard)
     context.user_data['product_id'] = update.message.text
-    await update.message.reply_text(f"Choose a color for product {update.message.text} {colors['hello']}:",
+    await update.message.reply_text(f"Выберите цвет товара:",
                                     reply_markup=reply_markup)
     return COLOR
 
 
 async def ask_for_size(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    cancel_button = InlineKeyboardButton("Cancel", callback_data='cancel_order')
+    cancel_button = InlineKeyboardButton("Отмена", callback_data='cancel_order')
     size_keyboard = [
         [InlineKeyboardButton("S", callback_data='size_s')],
         [InlineKeyboardButton("M", callback_data='size_m')],
@@ -46,12 +46,12 @@ async def ask_for_size(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
     color = query.data.split('_')[-1]
     context.user_data['color'] = color
     await update.callback_query.message.reply_text(
-        f"Choose a size for product {context.user_data['product_id']} with color {color}:", reply_markup=reply_markup)
+        f"Выберите размер товара:", reply_markup=reply_markup)
     return SIZE
 
 
 async def ask_for_address(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    cancel_button = InlineKeyboardButton("Cancel", callback_data='cancel_order')
+    cancel_button = InlineKeyboardButton("Отмена", callback_data='cancel_order')
     cancel_keyboard = [
         [cancel_button]
     ]
@@ -59,34 +59,37 @@ async def ask_for_address(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     query = update.callback_query
     size = query.data.split('_')[-1]
     context.user_data['size'] = size
-    await update.callback_query.message.reply_text("Write me your address.", reply_markup=reply_markup)
+    await update.callback_query.message.reply_text("Напишите адрес доставки.", reply_markup=reply_markup)
     return ADDRESS
 
 
 async def confirm_order(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     context.user_data['address'] = update.message.text
     await update.message.reply_text(
-        f"Your order was created. Product № {context.user_data['product_id']}, size: {context.user_data['size']}, color: {context.user_data['color']}, for address: {context.user_data['address']}")
+        f"Ваш заказ создан. \nПродукт № {context.user_data['product_id']}, размер: {context.user_data['size']}, цвет: {context.user_data['color']},\nадрес доставки: {context.user_data['address']} \nНомер заказа: 789")
     return ConversationHandler.END
 
 
 async def ask_for_order_id(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    cancel_button = InlineKeyboardButton("Cancel", callback_data='cancel_check')
+    cancel_button = InlineKeyboardButton("Отмена", callback_data='cancel_check')
     cancel_keyboard = [
         [cancel_button]
     ]
     reply_markup = InlineKeyboardMarkup(cancel_keyboard)
-    await update.message.reply_text("Send your order id.", reply_markup=reply_markup)
+    await update.message.reply_text("Отправьте номер вашего заказа.", reply_markup=reply_markup)
     return ORDER_ID
 
 
 async def send_order_status(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Your order is delivered!")
+    context.user_data['order_id'] = update.message.text
+    await update.message.reply_text(
+        f"Ваш заказ № {context.user_data['order_id']} в ожидании оплаты. \nПродукт № {context.user_data['product_id']}, размер: {context.user_data['size']}, цвет: {context.user_data['color']},\nадрес доставки: {context.user_data['address']}")
+
     return ConversationHandler.END
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     message = update.message or update.callback_query.message
     if message:
-        await message.reply_text("Operation canceled.")
+        await message.reply_text("Операция отменена.")
     return ConversationHandler.END
